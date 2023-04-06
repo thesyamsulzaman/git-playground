96a261552ea4956e62a50cc7c34cdf85a4bd7631
Syamsul Zaman <thesyamsulzaman@gmail.com>
2023-04-07 05:18:46 +0700

    05: initialize app

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 // This is from master branch
 const app = new App();
 
-app.init();
+// This is from feature 1 branch
+app.use(cors());
+app.use(localStore(__dirname + "local"));
+app.use(cloudService(new Docker()));
+
+const https = new Https();
+
+app.init(https);
